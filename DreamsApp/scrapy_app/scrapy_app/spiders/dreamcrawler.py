a35b45c94e4a5b89948a10baef7d68c1878e512a
Gene <jsiow002.dev@gmail.com>
2023-02-23 16:20:01 -0800

    modifications to dreamcrawler

--- DreamsApp/scrapy_app/scrapy_app/spiders/dreamcrawler.py
+++ DreamsApp/scrapy_app/scrapy_app/spiders/dreamcrawler.py
@@ -1,18 +1,50 @@
 import scrapy
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
-class DreamcrawlerSpider(CrawlSpider):
+"""
+class DreamcrawlerSpider(scrapy.Spider):
     name = "dreamcrawler"
     allowed_domains = ["www.dreambank.net"]
-    start_urls = ["http://www.dreambank.net/"]
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+    def start_requests(self):
+        urls = ["https://www.dreambank.net/random_sample.cgi?series=b&min=50&max=300&n=5000"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        self.logger("A response has arrived")
+        yield response.css('body span::text').getall()
+"""
+class DreamcrawlerSpider(scrapy.Spider):
+    name = "dreamcrawler"
 
-    def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+    def start_requests(self):
+        urls = ["https://www.dreambank.net/random_sample.cgi?series=b&min=50&max=300&n=5000"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    #ToDo:rename this function
+    #helper function
+    def pairwise(self,it):
+        it = iter(it)
+        while True:
+            try:
+                yield next(it) , next(it)
+            except StopIteration:
+                return
+    #ToDo: create a helper function that returns string, id, date
+    def parse(self, response):
+        data = []
+        raw_data = response.css('body span::text').getall()
+        for a,b in self.pairwise(raw_data):
+            #Split a into ID and date
+            id, date = a.split(" ")
+            string, id, date = re.sub("\(.*?\)|\[.*?\]","", b).strip() , id.replace("#","").strip() , re.sub(r'[()]','',date.strip())
+            data.append((id,date,string))
+        self.logger.info(data)
+        yield
+        {
+            'data': data,
+        }
