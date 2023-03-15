554a7fad9493f93ae996b017ef59604eba14ffd4
Gene <jsiow002.dev@gmail.com>
2023-03-14 23:36:19 -0700

    added changes to view, settings and serializers

--- DreamsApp/main/views.py
+++ DreamsApp/main/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from main.models import Dream
+from main.serializers import DreamSerializer
 
 # Create your views here.
+@csrf_exempt
+def dream_list(request):
+    """
+        List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        dreams = Dream.objects.all()
+        serializer = DreamSerializer(dreams, many = True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = DreamSerializer(data= data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors ,status=400)
+    
+    
