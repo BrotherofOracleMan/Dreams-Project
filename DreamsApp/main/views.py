404a8a9ebff55a9cfe2b3efbe9183abe2f6be6f2
Gene <jsiow002.dev@gmail.com>
2023-04-05 17:12:24 -0700

    Before and After Date implemented

--- DreamsApp/main/views.py
+++ DreamsApp/main/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from django_filters import rest_framework as filters
 from django.http import Http404
 from main.models import Dream
 from main.serializers import DreamSerializer
@@ -23,18 +24,29 @@ class ListDreambyID(APIView):
 
 #HTTP GET/v1/allquotes
 #HTTP GET /v1/quote/contain_string
-#HTTP GET /v1/before_date/{date}
-#HTTP GET /v1/after_date/{date}
 
 class GetAllQuotes(generics.ListAPIView):
     serializer_class = DreamSerializer
     def get_queryset(self):
         queryset = Dream.objects.all()
         contains_string = self.request.query_params.get('contains_string')
-        print(contains_string)
         if contains_string is not None:
             queryset = queryset.filter(quote__contains=contains_string)
         return queryset
+
+#HTTP GET /v1/before_date/{date}
+#HTTP GET /v1/after_date/{date}
+class GetDateFilter(filters.FilterSet):
+    date = filters.DateFromToRangeFilter()
+    class Meta: 
+        model = Dream
+        fields =['date']
+
+class ListDreamByDate(generics.ListAPIView):
+    queryset = Dream.objects.all()
+    serializer_class = DreamSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = GetDateFilter
 
 
 #POST(id, User, current date, quote)
