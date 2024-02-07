0996bbf3bb80929fd563d7e8baeec1dec66372bf
Gene <jsiow002.dev@gmail.com>
2024-02-07 12:00:39 -0800

    added get api parameters

--- DreamsProjectV2/backend/main.py
+++ DreamsProjectV2/backend/main.py
@@ -28,10 +28,18 @@ def get_record_by_dream_id(dream_id: int, db):
     return db.query(ModelDream).filter(ModelDream.id == dream_id).first() is not None
 
 
-# ToDo:create an API for getting a Dream from the DataBase
 @app.get("/")
 async def read_root():
     return {"message": "root index of API"}
+
+
+@app.get("/dreams")
+async def get_dream(dream: SchemaDream = None,
+                    dream_title: str = None,
+                    begin_date: datetime = None,
+                    last_date: datetime = None):
+    return {"message": "get item by query parameter"}
+
 
 # ToDo:create an API for updating a Dream from the DataBase
 @app.put("/dreams")
@@ -47,6 +55,7 @@ async def create_dream(dream: SchemaDream, db: Session = Depends(get_db)):
     }
     try:
         # first check if the dream exists within the DB
+        # need to add field validation
         if get_record_by_dream_id(dream.id, db) or get_record_by_dream_name(dream.dream_title, db):
             raise HTTPException(status_code=409, detail="Dream already exists")
         dream_dict = {**dream.dict(), 'last_updated': datetime.now(), 'created': datetime.now()}
